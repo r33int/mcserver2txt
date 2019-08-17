08d37661616f8490cc90da9a029eb2c7e5d1e8d1
Enzo <me@r33.space>
2019-08-17 15:10:40 +0200

    mcmod: Bump version to 1.0.1

--- src/main/java/mcserver2txt/Main.java
+++ src/main/java/mcserver2txt/Main.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main
 {
     public static final String MODID = "mcserver2txt";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
